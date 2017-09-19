b2e23562d88dfce844cb1a44299faf3b50a8002d
kczecior <kczecior@gmail.com>
2017-09-19 20:38:42 +0200

    main stworzenie instancji

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,8 @@
+import entity.Match;
 import entity.Player;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 /**
  * Created by RENT on 2017-09-19.
@@ -6,6 +10,24 @@ import entity.Player;
 public class Main {
     public static void main( String[] args ) {
 
-        HibernateDao<Player> participantHibernateDao = new HibernateDao<>(Player.class);
+        SessionFactory sessionFactory = SessionFactoryManager.getSessionFactory();
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        Player player1 = new Player("a","b","ab");
+        Player player2 = new Player("q","w","qw");
+        Player player3 = new Player("t","y","ty");
+        Player player4 = new Player("n","m","nm");
+
+        Match match1 = new Match(player1,player2);
+
+        session.save(player1);
+        session.save(player2);
+        session.save(player3);
+        session.save(player4);
+        session.save(match1);
+
+        transaction.commit();
+        session.close();
+        sessionFactory.close();
     }
 }
