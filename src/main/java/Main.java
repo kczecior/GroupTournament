1b877a9ae7b42e452ef8810f5294b159fe5db025
Your Name <you@example.com>
2017-09-19 20:19:00 +0200

    mergeehiber

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,11 @@
+import entity.Player;
+
 /**
  * Created by RENT on 2017-09-19.
  */
 public class Main {
     public static void main( String[] args ) {
 
+        HibernateDao<Player> participantHibernateDao = new HibernateDao<>(Player.class);
     }
 }
