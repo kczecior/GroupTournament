227658017648db7f3538ce177d6e9b646eb36188
kczecior <kczecior@gmail.com>
2017-09-20 17:42:35 +0200

    PAN DAREK

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,4 @@
-import entity.Match;
-import entity.Player;
-import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
 
 /**
  * Created by RENT on 2017-09-19.
@@ -11,23 +7,23 @@ public class Main {
     public static void main( String[] args ) {
 
         SessionFactory sessionFactory = SessionFactoryManager.getSessionFactory();
-        Session session = sessionFactory.openSession();
-        Transaction transaction = session.beginTransaction();
-        Player player1 = new Player("a","b","ab");
-        Player player2 = new Player("q","w","qw");
-        Player player3 = new Player("t","y","ty");
-        Player player4 = new Player("n","m","nm");
-
-        Match match1 = new Match(player1,player2);
-
-        session.save(player1);
-        session.save(player2);
-        session.save(player3);
-        session.save(player4);
-        session.save(match1);
-
-        transaction.commit();
-        session.close();
+//        Session session = sessionFactory.openSession();
+//        Transaction transaction = session.beginTransaction();
+//        Player player1 = new Player("a","b","ab");
+//        Player player2 = new Player("q","w","qw");
+//        Player player3 = new Player("t","y","ty");
+//        Player player4 = new Player("n","m","nm");
+//
+//        Match match1 = new Match(player1,player2);
+//
+//        session.save(player1);
+//        session.save(player2);
+//        session.save(player3);
+//        session.save(player4);
+//        session.save(match1);
+//
+//        transaction.commit();
+//        session.close();
         sessionFactory.close();
     }
 }
--- src/main/java/entity/Match.java
+++ src/main/java/entity/Match.java
@@ -1,99 +1,69 @@
 package entity;
 
 import javax.persistence.*;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Entity
+@Table(name = "`Match`")
 public class Match {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
     private int player1Score;
-    private int getPlayer2Score;
-    private boolean finished;
-    private int round;
-    private LocalDateTime occurenceDateTime;
+    private int player2Score;
     @ManyToOne
     private Player player1;
     @ManyToOne
     private Player player2;
+    private boolean finished;
 
-    public Match() {
-    }
+    private LocalDateTime occurrenceDateTime;
+    private int round;
 
-    public Match( Player player1, Player player2 ) {
+    public Match() {}
+
+    public Match(Player player1, Player player2, LocalDateTime occurrenceDateTime, int round) {
         this.player1 = player1;
+        this.player1.addMatchAsPlayer1(this);
         this.player2 = player2;
+        this.player2.addMatchAsPlayer2(this);
+        this.occurrenceDateTime = occurrenceDateTime;
+        this.round = round;
+        this.finished = false;
     }
 
     public int getPlayer1Score() {
         return player1Score;
     }
 
-    public void setPlayer1Score( int player1Score ) {
-        this.player1Score = player1Score;
-    }
-
-    public int getGetPlayer2Score() {
-        return getPlayer2Score;
-    }
-
-    public void setGetPlayer2Score( int getPlayer2Score ) {
-        this.getPlayer2Score = getPlayer2Score;
-    }
-
-    public boolean isFinished() {
-        return finished;
-    }
-
-    public void setFinished( boolean finished ) {
-        this.finished = finished;
-    }
-
-    public int getRound() {
-        return round;
-    }
-
-    public void setRound( int round ) {
-        this.round = round;
-    }
-
-    public LocalDateTime getOccurenceDateTime() {
-        return occurenceDateTime;
-    }
-
-    public void setOccurenceDateTime( LocalDateTime occurenceDateTime ) {
-        this.occurenceDateTime = occurenceDateTime;
+    public int getPlayer2Score() {
+        return player2Score;
     }
 
     public Player getPlayer1() {
         return player1;
     }
 
-    public void setPlayer1( Player player1 ) {
-        this.player1 = player1;
-    }
-
     public Player getPlayer2() {
         return player2;
     }
 
-    public void setPlayer2( Player player2 ) {
-        this.player2 = player2;
+    public LocalDateTime getOccurrenceDateTime() {
+        return occurrenceDateTime;
     }
 
-//    @Entity
-//    public class Customer implements java.io.Serializable {
-//
-//        private Address address;
-//
-//        @OneToOne(cascade={CascadeType.ALL})
-//        @JoinColumn(name="ADDRESS_ID")
-//        public Address getAddress( ) {
-//            return address;
-//        }
-//
-//    }
+    public int getRound() {
+        return round;
+    }
+
+    public boolean isFinished() {
+        return finished;
+    }
+
+    public void setFinalScore(int player1Score, int player2Score) {
+        this.player1Score = player1Score;
+        this.player2Score = player2Score;
+        this.finished = true;
+    }
 }
